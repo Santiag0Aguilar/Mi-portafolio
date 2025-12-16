7a8a2742bad96dcb3433dae05bd379e58941abf2
Santiago Aguilar <130328638+Santiag0Aguilar@users.noreply.github.com>
2025-12-15 19:03:15 -0600

    Add files via upload

--- build/scripts/translations.js
+++ build/scripts/translations.js
@@ -7,7 +7,7 @@ const translations = {
     hireMe: "Contrátame",
     skills: "Mis habilidades",
     paragraph1: `Me apasiona el desarrollo web y la tecnología, actualmente estoy en mi primer año de universidad. He desarrollado aplicaciones web completas, integrando tanto el diseño de interfaz como la lógica del servidor. Busco una oportunidad como Desarrollador Full Stack Junior para seguir creciendo profesionalmente.`,
-    paragraph2: `¿Buscas un desarrollador que convierta tu idea en realidad y ayude a hacer crecer tu negocio? Trabajemos juntos.`,
+    paragraph2: `Busco una oportunidad como desarrollador web junior donde pueda aportar mis conocimientos y seguir creciendo profesionalmente en un entorno colaborativo.`,
     downloadCV: "Descargar CV",
     state: "Estado de México",
     letsChat: "Hablemos",
@@ -32,7 +32,9 @@ const translations = {
     hireMe: "Hire me",
     skills: "My skills",
     paragraph1: `I am passionate about web development and technology, currently in my first year of university. I have developed complete web applications, integrating both interface design and server-side logic. I am seeking an opportunity as a Junior Full Stack Developer to continue growing professionally.`,
-    paragraph2: `Are you looking for a developer to turn your idea into reality and help grow your business? Let's work together to make it happen.`,
+    paragraph2: `I'm looking for an opportunity as a Junior Web Developer where I
+              can contribute my skills and continue growing professionally in a
+              collaborative environment.`,
     downloadCV: "Download CV",
     state: "State of Mexico",
     letsChat: "Let's chat",
